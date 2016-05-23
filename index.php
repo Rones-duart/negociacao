e680e4a8b22c1866843f27a374757da1d8366ee2
Rones-duart <ronesduart@hotmail.com>
2016-05-23 11:44:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<!-- Desenvolvido por Rones Duarte-->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Escolha uma opção! </title>
+		<link rel= "stylesheet" type="text/css" href="css/mystyle.css"/>
+		<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+		
+	
+	</head>
+	
+	<body>
+		<div id="top"></div>
+		
+			<div id="home">		
+				<!--botões de acesso a aplicação -->	
+				<div id="opcao1" class= "bt bradius"><a href="inserir.php" title=""><b>Cadastrar Mecadorias</b></a></div>
+				<div id="opcao2" class= "bt bradius"><a href="visualizar.php" title=""><b>Visualizar Mecadorias</b></a></div>
+				
+			</div>
+				
+		</div>
+	</body>
+</html>
